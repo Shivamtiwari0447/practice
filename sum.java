4e425e837f4312373e822164d4931b055c3d4f33
Shivam Tiwari <Shivamtiwari0447.com>
2022-09-14 09:14:49 +0530

    commiting java file

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+Class Sum{
+public static void main(String argc[]){
+	int a=5,b=6,c;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter first number");
+	c=a+b;
+	System.out.println("sum is: "+c);
+}
+}
