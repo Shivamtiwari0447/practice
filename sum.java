d799be1da908fae681a1d676c8a86949d99395dd
Shivam Tiwari <Shivamtiwari0447.com>
2022-09-14 09:29:56 +0530

    message

--- sum.java
+++ sum.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
-Class Sum{
-public static void main(String argc[]){
-	int a=5,b=6,c;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("enter first number");
-	c=a+b;
-	System.out.println("sum is: "+c);
-}
+public class Sum
+{
+	public static void main(String argc[]){
+		int a=5,b=6,c;
+		System.out.println("enter first number");
+		System.out.println("enter second number");
+		c=a+b;
+		System.out.println("sum is: "+c);
+	}
 }
